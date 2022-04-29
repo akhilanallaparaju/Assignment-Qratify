89046d059df8c50e61b6600ff9b4eb0df240650f
Akhila Nallaparaju <akhila.nallaparaju19@gmail.com>
2022-04-29 12:50:18 +0200

    My app draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import {NestedTreeControl} from '@angular/cdk/tree';
+import {Component} from '@angular/core';
+import {MatTreeNestedDataSource} from '@angular/material/tree';
+import { HttpClient } from '@angular/common/http';
+
+
+ interface Node {
+  name: string;
+  children?: Node[];
+}
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +17,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assignment-Qratify';
+  message = "Nothing Selected"
+  selectedName = ""
+
+  treeControl = new NestedTreeControl<Node>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<Node>();
+
+  constructor(private http: HttpClient) {
+    this.getTreeStructure().subscribe(res =>  this.dataSource.data = res);
+  }
+
+  hasChild = (_: number, node: Node) => !!node.children && node.children.length > 0;
+
+  updateSelectedName(name: string){
+    this.message = name + " Selected"
+    this.selectedName = name
+  }
+  getTreeStructure(){
+    const url = 'https://61f5037b62f1e300173c3f8d.mockapi.io/node'
+    return this.http.get<Node[]>(url)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTreeModule} from '@angular/material/tree';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatTreeModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
